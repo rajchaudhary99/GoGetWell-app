69e60be04f7557d2f936385118adbfc654556a91
RAJ VARDHAN CHAUDHARY <112545110+rajchaudhary99@users.noreply.github.com>
2025-04-27 11:14:59 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App'
-import './styles/base.css'
+import './Styles/base.css'
 
 
 const root = createRoot(document.getElementById('root'))
